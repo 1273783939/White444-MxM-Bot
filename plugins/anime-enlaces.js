3bf28752fad27413bd27d6d52e6441e1f1cc4a97
Prakharper <158788969+Prakharper@users.noreply.github.com>
2025-01-19 19:00:05 -0400

    Actualizar anime-enlaces.js

--- plugins/anime-enlaces.js
+++ plugins/anime-enlaces.js
@@ -2,19 +2,19 @@ var handler = async m =>
 
 conn.reply(m.chat, ` 
 ┏━━━━━━━━━━━━━━━━━━━━┅
-┇                   *「 ANIME」*       
+┇                   *「𝐀𝐍𝐈𝐌𝐄」*       
 ┣━━━━━━━━━━━━━━━━━━━━┅
-┃ ❖ https://kusonime.com
-┃ ❖ https://huntersekaisub.blogspot.com
-┃ ❖ https://riie.jp
-┃ ❖ https://m.meownime.ai
-┃ ❖ https://meownime.ltd
-┃ ❖ https://nimegami.id
-┃ ❖ https://animekompi.cam
-┃ ❖ https://nontonanimeid.top
-┃ ❖ https://kazefuri.vip
-┃ ❖ https://pendekarsubs.us
-┃ ❖ https://myanimelist.net
+┃ ☠︎︎ https://kusonime.com
+┃ ☠︎︎ https://huntersekaisub.blogspot.com
+┃ ☠︎︎ https://riie.jp
+┃ ☠︎︎ https://m.meownime.ai
+┃ ☠︎︎ https://meownime.ltd
+┃ ☠︎︎ https://nimegami.id
+┃ ☠︎︎ https://animekompi.cam
+┃ ☠︎︎ https://nontonanimeid.top
+┃ ☠︎︎ https://kazefuri.vip
+┃ ☠︎︎ https://pendekarsubs.us
+┃ ☠︎︎ https://myanimelist.net
 ┗━━━━━━━━━━━━━━━━━━━━┅
  `, m, rcanal, )
 
