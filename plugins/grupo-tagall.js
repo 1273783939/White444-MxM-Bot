21b1707b11afeae5311d9f4868a3ff6b900f58c6
Prakharper <158788969+Prakharper@users.noreply.github.com>
2025-01-19 21:53:47 -0400

    Actualizar grupo-tagall.js

--- plugins/grupo-tagall.js
+++ plugins/grupo-tagall.js
@@ -6,7 +6,7 @@
 const handler = async (m, { isOwner, isAdmin, conn, text, participants, args, command, usedPrefix }) => {
   if (usedPrefix == 'a' || usedPrefix == 'A') return;
 
-  const customEmoji = global.db.data.chats[m.chat]?.customEmoji || '🤍';
+  const customEmoji = global.db.data.chats[m.chat]?.customEmoji || '❏◢ •.';
   m.react(customEmoji);
 
   if (!(isAdmin || isOwner)) {
@@ -16,7 +16,7 @@ const handler = async (m, { isOwner, isAdmin, conn, text, participants, args, co
 
   const pesan = args.join` `;
   const oi = `*» INFO :* ${pesan}`;
-  let teks = `*!  MENCION GENERAL  !*\n  *PARA ${participants.length} MIEMBROS* 🗣️\n\n ${oi}\n\n╭  ┄ 𝅄  ۪꒰ \`⡞᪲=͟͟͞🄶𝚎᪶۫۫𝚗᪶۫۫𝚎᪶۫۫𝚜᪶۫۫𝚒᪶۫۫𝚜᪶۫ 𝚊᪶۫𝚒᪶۫͜ ≼᳞ׄ\` ꒱  ۟  𝅄 ┄\n`;
+  let teks = `*!     ꯭꯭𝗗𝗘𝗦𝗣𝗜𝗘𝗥𝗧𝗘𝗡  !*\n  *❲ 𝝦 𝗘 Ⲥ 𝖠 𝖣 𝝤 𝙍 𝗘 𝗦 貝 ❳ ₊ ${participants.length} * 🩸\n\n ${oi}\n\n╭  ┄ 𝅄  ۪꒰ \`⡞᪲=͟͟͞🄶𝚎᪶۫۫𝚗᪶۫۫𝚎᪶۫۫𝚜᪶۫۫𝚒᪶۫۫𝚜᪶۫ 𝚊᪶۫𝚒᪶۫͜ ≼᳞ׄ\` ꒱  ۟  𝅄 ┄\n`;
   for (const mem of participants) {
     teks += `┊${customEmoji} @${mem.id.split('@')[0]}\n`;
   }
