2060622e564418553db63a6145f2485d127d7df4
Prakharper <158788969+Prakharper@users.noreply.github.com>
2025-01-19 22:22:04 -0400

    Actualizar info-bot.js

--- plugins/info-bot.js
+++ plugins/info-bot.js
@@ -6,7 +6,7 @@ handler.all = async function(m) {
 
   if (/^bot$/i.test(m.text) && !chat.isBanned) {
     conn.sendPresenceUpdate('recording', m.chat);
-conn.reply(m.chat, `🤍 ¡Hola! Soy Ai Genesis, en que puedo ayudarte hoy?\n\n✰ Usa *!menu* para ver mis comandos.`, m, rcanal, )
+conn.reply(m.chat, `🩸 ¡Hola! Soy Demon 2.2, en que puedo ayudarte hoy?\n\n✰ Usa *!menu* para ver mis comandos.`, m, rcanal, )
 }
 if (/^sexo$/i.test(m.text)) {
 conn.reply(m.chat, `*pervertido* 🫣`, m, rcanal, )
