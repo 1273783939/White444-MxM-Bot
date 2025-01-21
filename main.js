93171ea200e60be2f17bfe50cdcdebe743a36694
Prakharper <158788969+Prakharper@users.noreply.github.com>
2025-01-20 22:20:25 -0400

    Actualizar main.js

--- main.js
+++ main.js
@@ -155,17 +155,17 @@ let numeroTelefono
 if (!!phoneNumber) {
 numeroTelefono = phoneNumber.replace(/[^0-9]/g, '')
 if (!Object.keys(PHONENUMBER_MCC).some(v => numeroTelefono.startsWith(v))) {
-console.log(chalk.bgBlack(chalk.bold.blueBright(`🤍 Por favor, Ingrese el número de WhatsApp.\n${chalk.bold.yellowBright(`☁️  Ejemplo: 57321×××××××`)}\n`)))
+console.log(chalk.bgBlack(chalk.bold.blueBright(`🩸 Por favor, Ingrese el número de WhatsApp.\n${chalk.bold.yellowBright(`🩸  Ejemplo: 57321×××××××`)}\n`)))
 process.exit(0)
 }} else {
 while (true) {
-numeroTelefono = await question(chalk.bgBlack(chalk.bold.blueBright(`🤍 Por favor, escriba su número de WhatsApp.\n☁️  Ejemplo: 57321×××××××\n`)))
+numeroTelefono = await question(chalk.bgBlack(chalk.bold.blueBright(`🩸 Por favor, escriba su número de WhatsApp.\n🩸  Ejemplo: 57321×××××××\n`)))
 numeroTelefono = numeroTelefono.replace(/[^0-9]/g, '')
 
 if (numeroTelefono.match(/^\d+$/) && Object.keys(PHONENUMBER_MCC).some(v => numeroTelefono.startsWith(v))) {
 break 
 } else {
-console.log(chalk.bgBlack(chalk.bold.blueBright(`🤍 Por favor, escriba su número de WhatsApp.\n☁️  Ejemplo: 57321×××××××\n`)))
+console.log(chalk.bgBlack(chalk.bold.blueBright(`🩸 Por favor, escriba su número de WhatsApp.\n🩸  Ejemplo: 57321×××××××\n`)))
 }}
 rl.close()  
 } 
@@ -173,14 +173,14 @@ rl.close()
 setTimeout(async () => {
 let codigo = await conn.requestPairingCode(numeroTelefono)
 codigo = codigo?.match(/.{1,4}/g)?.join("-") || codigo
-console.log(chalk.bold.white(chalk.bgBlue(`🤍 SU CÓDIGO:`)), chalk.bold.white(chalk.white(codigo)))
+console.log(chalk.bold.white(chalk.bgBlue(`🩸 SU CÓDIGO:`)), chalk.bold.white(chalk.white(codigo)))
 }, 3000)
 }}
 }
 
 conn.isInit = false;
 conn.well = false;
-//conn.logger.info(`🔵  H E C H O\n`)
+//conn.logger.info(`🚀  H E C H O\n`)
 
 if (!opts['test']) {
 if (global.db) setInterval(async () => {
