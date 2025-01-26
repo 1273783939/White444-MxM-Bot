0e6f972d40cb608a34d3746b777731344d343334
Prakharper <158788969+Prakharper@users.noreply.github.com>
2025-01-26 19:57:59 -0400

    Actualizar _welcome.js

--- plugins/_welcome.js
+++ plugins/_welcome.js
@@ -66,7 +66,7 @@ export async function before(m, { conn, participants, groupMetadata }) {
   }
 
   if (chat.welcome && m.messageStubType == 32) {
-    let kick = `🩸 *Se salió*  del grupo  *${groupMetadata.subject.trim()}*\n    ㌃@${m.messageStubParameters[0].split`@`[0]}\n\n   愛 ────㌃ Un inútil más que se sale del grupo, esperemos nunca vuelva.\n\n> ✐ No olvides usar *#help* si necesitas algo.\n> 🜸 Adiós...`;
+    let kick = `🩸 *Se salió*  del grupo  *${groupMetadata.subject.trim()}*\n    ㌃@${m.messageStubParameters[0].split`@`[0]}\n\n   愛 ────㌃ Un inútil más que se sale del grupo, esperemos nunca vuelva.\n\n> ✐ No olvides usar *#help* si necesitas algo.\n> 🜸 No eres bienvenido.`;
 
     let img = await generateImage(
       '¡ADIOS!',
